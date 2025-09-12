4358275653707a0ca46f19428e4b170b74395992
Humayun <humayunwahid70@gmail.com>
2025-09-13 00:02:56 +0500

    Blogs WOrk

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "blogs.globirix.com",
+      },
+      {
+        protocol: "https",
+        hostname: "secure.gravatar.com", // for author_pic
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
